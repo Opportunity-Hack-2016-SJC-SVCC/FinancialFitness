05024e624b6077f2bf5e7ab0ab7a43c63584d677
Ankit Jasuja <AJasuja@walmartlabs.com>
2016-09-24 21:17:31 -0700

    added all storyboards and initial view controllers

---
+++ FinancialFitness/Constants.swift
@@ -0,0 +1,18 @@
+//
+//  Constants.swift
+//  FinancialFitness
+//
+//  Created by Ankit Jasuja on 9/24/16.
+//  Copyright © 2016 Ashish Mishra. All rights reserved.
+//
+
+import UIKit
+
+class Constants: NSObject {
+
+    // ==================== Segue =====================
+    static let QUESTION_TO_RESOURCE_SEGUE = "Question2ResouceSegue"
+    static let RESOURCE_VEDIO_SEGUE = "ResourceVedioSegue"
+    static let RESOURCE_TIPS_SEGUE = "ResourceTipsSegue"
+    static let RESOURCE_SERVICES_SEGUE = "ResourceServicesSegue"
+}
