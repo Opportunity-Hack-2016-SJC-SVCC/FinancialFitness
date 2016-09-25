196d259aff1aeee053ed67ea5eeeb13ad6463725
ashish816 <ashish88.it@gmail.com>
2016-09-24 22:44:42 -0700

    ResourceTableViewCell added.

---
+++ FinancialFitness/ResourcesTableViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  ResourcesTableViewCell.swift
+//  FinancialFitness
+//
+//  Created by Ashish Mishra on 9/24/16.
+//  Copyright © 2016 Ashish Mishra. All rights reserved.
+//
+
+import UIKit
+
+class ResourcesTableViewCell: UITableViewCell {
+    
+    @IBOutlet var resourceTypeLabel : UILabel!
+    @IBOutlet var resourceDescriptionlabel : UILabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
